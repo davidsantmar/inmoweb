dd86433a66e34fb1aa29a3c37e708d6bc043bcac
David <davidsantmar@gmail.com>
2022-04-12 16:51:55 +0200

    added header and body font

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
 import './App.css';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { Provider } from "react-redux";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Provider } from 'react-redux';
 import Inmuebles from './components/Inmuebles';
 import Empresa from './components/Empresa';
 import Hipotecas from './components/Hipotecas';
 import Ayuda from './components/Ayuda';
 import Intranet from './components/Intranet';
+import store from './redux/stores';
+import Header from './components/Header';
+
+
 
 function App() {
   return (
-    <Provider store={store}>
+    <Header />
+    
+  )
+   /** <Provider store={store}>
     <BrowserRouter>
       <main>
-        <Switch>
+        <Routes>
           <Route path="/" exact component={Inmuebles} />
           <Route path="/empresa" exact component={Empresa} />
           <Route path="/ayuda" exact component={Ayuda} />
           <Route path="/hipotecas" exact component={Hipotecas} />
           <Route path="/intranet" exact component={Intranet} />
-        </Switch>
+        </Routes>
       </main>
     </BrowserRouter>
-  </Provider>
-  );
+  </Provider>*/
+  
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <>
+        <div className='header--container'>
+            <div>
+                Home
+            </div>
+            <div>
+                Sobre nosotros
+            </div>
+            <div>
+                Hipotecas
+            </div>
+            <div>
+                Inmuebles
+            </div>
+        </div>
+        </>
+    );
+};
+
+export default Header;
