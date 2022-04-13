48f04ee35aae2e93b6d1fc1cfae1d268931ac9d9
David <davidsantmar@gmail.com>
2022-04-13 17:44:37 +0200

    header links amended

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import logo from '../images/logo.png';
 
 const Header = () => {
     return (
         <>
         <div className='header--container'>
-            <Link to="/" className="nav--link logo">
+            <Link to="/" className="nav__link">
+                <img className='logo' src={logo} alt='logo'/>
             </Link>
-            <Link to="/about" className="nav--link">
+            <Link to="/about" className="nav__link">
                 Sobre nosotros
             </Link>
-            <Link to="/hipotecas" className="nav--link">
+            <Link to="/hipotecas" className="nav__link">
                 Hipotecas
             </Link>
-            <Link to="/contacto" className="nav--link">
+            <Link to="/contacto" className="nav__link">
                 Contacto
             </Link>
         </div>
