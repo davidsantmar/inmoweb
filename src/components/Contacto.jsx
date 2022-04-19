74ff55dac1473bd3fec39bd64fceade1056a3b5f
David <davidsantmar@gmail.com>
2022-04-19 22:39:48 +0200

    added email sended form

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -1,6 +1,35 @@
 import React from 'react';
+import { useState } from 'react';
+import { send } from 'emailjs-com';
+
 
 const Contacto = () => {
+    const [toSend, setToSend] = useState({
+        from_name: '',
+        to_name: '',
+        message: '',
+        reply_to: '',
+      });
+    
+      const onSubmit = (e) => {
+        e.preventDefault();
+        send(
+            'service_6ilyu6d',
+            'template_h7gucd8',
+            toSend,
+            'mZFlYH32MWGNn7UKB'
+          )
+            .then((response) => {
+              console.log('SUCCESS!', response.status, response.text);
+            })
+            .catch((err) => {
+              console.log('FAILED...', err);
+            });     
+        };
+    
+      const handleChange = (e) => {
+        setToSend({ ...toSend, [e.target.name]: e.target.value });
+      };
     return (
         <>
             <div className='sub--title'>
@@ -20,21 +49,43 @@ const Contacto = () => {
             <div className='sub--title'>
                 <h4>CONTACT FORM</h4>
             </div>
-            <form className='form--container'>
-                <label for="fname">Name: </label>
-                <br />
-                <input className='name__field' type="text" placeholder='Type your name'></input>
-                <br />
-                <label for="email">Email: </label>
-                <br />
-                <input className='email__field' type="email" placeholder='@'></input>
-                <br />
-                <label for="request">Request: </label>
-                <br />
-                <input className='request__field' type="text" id="request" name="request" ></input>
-                <br />
-                <br />
-                <div className='submit__button'>Submit</div>
+            <form className='form--container' onSubmit={onSubmit}>
+                <input
+                    className='name__field'
+                    type='text'
+                    name='from_name'
+                    placeholder='from name'
+                    value={toSend.from_name}
+                    onChange={handleChange}
+                />
+                <input
+                    type='text'
+                    name='to_name'
+                    placeholder='to name'
+                    value={toSend.to_name}
+                    onChange={handleChange}
+                />
+                <input
+                    className='request__field'
+                    type='text'
+                    name='message'
+                    placeholder='Your message'
+                    value={toSend.message}
+                    onChange={handleChange}
+                />
+                <input
+                    className='email__field'
+                    type='text'
+                    name='reply_to'
+                    placeholder='Your email'
+                    value={toSend.reply_to}
+                    onChange={handleChange}
+                />
+                <button 
+                    className='submit__button' 
+                    type='submit'
+                />
+                    Submit
             </form>
         </>
     );
