e0a127807e21155edf68baba78a22e727cf8dca9
David <davidsantmar@gmail.com>
2022-04-28 22:04:54 +0200

    added footer test

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className="footer--container">
-            <footer>
+        <div className='footer--container'>
+            <footer data-testid='footerText'>
                 Made with &#10084;&#65039; by David SantiaGO!
             </footer>
         </div>
