c352fff9d6665bfd34b19f5c453f13a4e473e26d
David <davidsantmar@gmail.com>
2022-05-18 22:30:37 +0200

    added PA price field

--- src/components/PA.jsx
+++ src/components/PA.jsx
@@ -3,6 +3,7 @@ import React, {useState} from 'react';
 const PA = () => {
     const [rooms, setRooms] = useState(1);
     const [m2, setM2] = useState(60);
+    const [price, setPrice] = useState(100000);
 
     const addRooms = () => {
         setRooms(rooms + 1);
@@ -16,7 +17,12 @@ const PA = () => {
     const removeM2 = () => {
         setM2(m2 - 1);
     }
-
+    const addPrice = () => {
+        setPrice(price + 1000);
+    }
+    const removePrice = () => {
+        setM2(price - 1000);
+    }
     return (
         <div className='PA--form'>
             <div className='sub--title' data-testid='subTitle'>
@@ -55,6 +61,16 @@ const PA = () => {
                         <span className='change__numbers__buttons' onClick={removeM2}>-</span>
                         {m2}
                         <span className='change__numbers__buttons' onClick={addM2}>+</span>                
+                    </div>
+                </div>
+                <br />
+                <br />
+                <div className='price--title' data-testid='price-title'>
+                    m2
+                    <div className='price--container' data-testid='price-container'>
+                        <span className='change__numbers__buttons' onClick={removePrice}>-</span>
+                        {price}
+                        <span className='change__numbers__buttons' onClick={addPrice}>+</span>                
                     </div>
                 </div>
                 <br />
