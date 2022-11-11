b363457dd5a09cf257b857ac6ceac3eac246ce86
David <davidsantmar@gmail.com>
2022-11-11 17:08:45 +0100

    hamburguer menu working

--- src/components/Hamburguer.jsx
+++ src/components/Hamburguer.jsx
@@ -22,8 +22,9 @@ const Hamburguer = () => {
         const data = await getDocs(usersCollection);
         setUsers(data.docs.map((doc) => ({...doc.data(), id:doc.id})));
       }
-      getUsers();  
+      getUsers(); 
     }, []);
+    
     async function handleLogin(){
       const usuarios = [];
       const emails = [];
@@ -43,40 +44,73 @@ const Hamburguer = () => {
     function handleLogout() {
       dispatch(logout());
     }
+    const closeMenu = () => {
+     
+      document.getElementById('menu-toggle').checked = false;
+    //document.getElementById('hamburguer-container').addEventListener('click',function(){
+      //document.getElementById('menu-toggle').checked === false;
+          
+      
+    
+    
+    }
+
     return (
         <>
-           <section className="hamburguer--container">
+           <section className='hamburguer--container' id='hamburguer-container'>
                 <div>
-                    <Link to="/" className="nav__link" data-testid='logo-link'>
-                    <img className='logo' src={logo} alt='logo'/>
+                    <Link to='/' className='nav__link logo--container' data-testid='logo-link'>
+                        <img className='logo' src={logo} alt='logo'/>
                     </Link>                
                 </div>
-                <input id="menu-toggle" type="checkbox" />
-                <label className='menu-button-container' htmlFor="menu-toggle">
+                <input id='menu-toggle' type='checkbox' />
+                <label className='menu-button-container' htmlFor='menu-toggle'>
                 <div className='menu-button'>
                 </div>
                 </label>
-                <ul className="menu">
-                    <li>
-                        <Link to="/about" className="nav__link about__us__link" data-testid='about-us-link'>
-                            <span className='nav__about__us'>About us</span>
+                <ul className='menu'>
+                    <li className='menu__option'>
+                        <Link to='/about' className='nav__link' data-testid='about-us-link' onClick={closeMenu}>
+                            About us
                         </Link>
                     </li>
-                    <li>
-                        <Link to="/mortgages" className="nav__link" data-testid='hipotecas-link'>
+                    <li className='menu__option'>
+                        <Link to='/mortgages' className='nav__link' data-testid='hipotecas-link' onClick={closeMenu}>
                             Mortgages
                         </Link>
                     </li>
-                    <li>
-                        <Link to="/contact" className="nav__link" data-testid='contacto-link'>
+                    <li className='menu__option' onClick={closeMenu}>
+                        <Link to='/contact' className='nav__link' data-testid='contacto-link' onClick={closeMenu}>
                             Contact
                         </Link>
                     </li>
-                    <li>
-                        <Link to="/" onClick={handleLogout} type="button" className="logout__button"  data-testid='logout-button'>
-                            &#x23FB;
-                        </Link>
+                    {isAuthenticated ? (
+                    <>
+                    <li className='menu__option'>
+                      <Link to='/createProperty' className='nav__link' onClick={closeMenu}>
+                          Admin
+                      </Link> 
                     </li>
+                    <li className='nav__link logout__button'
+                      onClick={handleLogout}
+                        type='button'
+                        data-testid='logout-button'
+                      >
+                        <Link to='/' className='nav__link__logout'>
+                          <span className='off__symbol' onClick={closeMenu}>&#x23FB;</span>
+                        </Link> 
+                    </li>
+                  </>
+                  ) : (
+                  <li
+                    onClick={handleLogin}
+                    type='button'
+                    className='login__button'
+                    data-testid='login-button'
+                  >
+                    <span className='on__symbol'>&#x23FB;</span>
+                  </li>
+                )}
                 </ul>
             </section>
         </>
